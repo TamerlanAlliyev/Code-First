c6a1cf5212f673b376a87945d2cd4839c1bab3a5
TamerlanAlliyev <tamerlanma@code.edu.az>
2024-03-06 01:05:55 +0400

    all file removed

--- EfCodeFirst/Models/Entities/SpecificationValue.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EfCodeFirst.Models.Entities
-{
-    public class SpecificationValue
-    {
-        public int SpecificationId { get; set; }
-        public int ProductId { get; set; }
-        public string Value { get; set; }
-    }
-}
